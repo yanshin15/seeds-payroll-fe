97ca9c437d3b5b0d90486c544284d4e99ca05f1a
yanshin <yan.shin@cdsg.com.mm>
2023-10-12 11:11:13 +0630

    Structure for home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import {
-  faCirclePlus,
-  faHandHoldingDollar,
-  faSackDollar,
-  faMoneyBill1,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlus, faMoneyBill1 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import TeacherDisplay from "../components/TeacherDisplay";
 
@@ -19,9 +14,8 @@ const HomePage = () => {
       setIsLoading(true);
       const response = await axios.get("http://localhost:5038/api/teachers/");
       console.log(response.data);
-      console.log("cat");
-      // console.log(response.data);
       setProducts(response.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -33,26 +27,32 @@ const HomePage = () => {
 
   return (
     <div className=" bg-lightcream px-4 w-full h-full flex flex-col">
-      <span className="flex justify-between gap-8 p-2 max-md:flex-col px-6">
-        <Link to="/create">
-          <FontAwesomeIcon
-            icon={faCirclePlus}
-            className="h-10 text-darkbrown hover:scale-105 hover:text-seedsbrown duration-200"
-          />
-        </Link>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <>
+          <span className="flex justify-between gap-8 p-2 px-6">
+            <Link to="/create">
+              <FontAwesomeIcon
+                icon={faCirclePlus}
+                className="h-8 text-darkbrown hover:scale-105 hover:text-seedsbrown duration-200"
+              />
+            </Link>
 
-        <Link to="/create">
-          <FontAwesomeIcon
-            icon={faMoneyBill1}
-            className="h-10 text-darkbrown hover:scale-105 hover:text-seedsbrown duration-200"
-          />
-        </Link>
-      </span>
-      <div className=" grid lg:grid-cols-2 md:grid-cols-1 gap-8 mt-4 select-none">
-        {products.map((product) => {
-          return <TeacherDisplay product={product} />;
-        })}
-      </div>
+            <Link to="/create">
+              <FontAwesomeIcon
+                icon={faMoneyBill1}
+                className="h-8 text-darkbrown hover:scale-105 hover:text-seedsbrown duration-200"
+              />
+            </Link>
+          </span>
+          <div className=" grid lg:grid-cols-2 md:grid-cols-1 gap-8 mt-4 mb-4 select-none">
+            {products.map((product) => {
+              return <TeacherDisplay product={product} />;
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 };
