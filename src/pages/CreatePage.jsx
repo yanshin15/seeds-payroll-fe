2967f9c8609dc3d435309d78b69538d30efc8d0b
yanshin <yan.shin@cdsg.com.mm>
2023-10-12 11:36:50 +0630

    Add layout for CreatePage

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 const CreatePage = () => {
-  return <div className="px-4 py-2">CreatePage</div>;
+  return (
+    <section className="bg-white px-4 w-full h-screen flex flex-col items-center p-5">
+      <form
+        action=""
+        className="shadow-xl rounded-xl bg-lightcream p-8 text-2xl items-center w-2/5 h-auto flex flex-col gap-4"
+      >
+        <h1 className="text-darkbrown text-3xl font-bold mb-6">
+          Add a Teacher
+        </h1>
+        <label>Name</label>
+        <input
+          type="text"
+          className="w-3/4 h-12 px-8 py-4 rounded-2xl shadow "
+          placeholder="Enter Name"
+        />
+        <label>Subject</label>
+        <input
+          type="text"
+          className="w-3/4 h-12 px-8 py-4 rounded-2xl shadow"
+          placeholder="Enter Subject"
+        />
+        <label>Rate</label>
+        <input
+          type="text"
+          className="w-3/4 h-12 px-8 py-4 rounded-2xl shadow"
+          placeholder="Enter Rate"
+        />
+        <button className="duration-200 text-center w-3/4 font-semibold  bg-darkgreen px-4 py-2 rounded-lg hover:bg-seedsgreen text-white mt-12">
+          Edit
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePage;
--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -2,29 +2,43 @@ import React from "react";
 
 const EditPage = () => {
   return (
-    <div className="bg-white px-4 w-full h-screen flex flex-col items-center p-5">
+    <section className="bg-white px-4 w-full h-screen flex flex-col items-center p-5">
       <form
         action=""
-        className="shadow-xl rounded-xl bg-lightcream p-8 text-2xl items-center w-2/5 h-4/5 flex flex-col gap-8"
+        className="shadow-xl rounded-xl bg-lightcream p-8 text-2xl items-center w-2/5 h-auto flex flex-col gap-4"
       >
+        <h1 className="text-darkbrown text-3xl font-bold mb-6">
+          Edit teacher details
+        </h1>
+        <label>Name</label>
         <input
           type="text"
-          className="w-3/4 h-1/6 px-4 py-4 rounded-2xl shadow"
+          className="w-3/4 h-12 px-8 py-4 rounded-2xl shadow "
+          placeholder="Enter Name"
         />
+        <label>Subject</label>
         <input
           type="text"
-          className="w-3/4 h-1/6 px-4 py-4 rounded-2xl shadow"
+          className="w-3/4 h-12 px-8 py-4 rounded-2xl shadow"
+          placeholder="Enter Subject"
         />
+        <label>Rate</label>
         <input
           type="text"
-          className="w-3/4 h-1/6 px-4 py-4 rounded-2xl shadow"
+          className="w-3/4 h-12 px-8 py-4 rounded-2xl shadow"
+          placeholder="Enter Rate"
         />
+        <label>Hours Worked</label>
         <input
           type="text"
-          className="w-3/4 h-1/6 px-4 py-4 rounded-2xl shadow"
+          className="w-3/4 h-12 px-8 py-4 rounded-2xl shadow"
+          placeholder="Enter Hours worked."
         />
+        <button className="duration-200 text-center w-3/4 font-semibold  bg-darkgreen px-4 py-2 rounded-lg hover:bg-seedsgreen text-white mt-12">
+          Edit
+        </button>
       </form>
-    </div>
+    </section>
   );
 };
 
