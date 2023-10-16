950540b790dea05144b638d78fe06425a99b853d
yanshin <yan.shin@cdsg.com.mm>
2023-10-16 16:56:04 +0630

    Sample structure for table in Payroll

---
+++ src/components/TeacherTable.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TeacherTable = (teacher) => {
+  return (
+    <div>
+      <h1>{teacher.name}</h1>
+    </div>
+  );
+};
+
+export default TeacherTable;
--- src/pages/PayrollPage.jsx
+++ src/pages/PayrollPage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import TeacherTable from "../components/TeacherTable";
 
 const PayrollPage = () => {
   const [teachers, setTeachers] = useState([]);
@@ -20,16 +21,46 @@ const PayrollPage = () => {
   useEffect(() => {
     getPayroll();
   }, []);
-  return teachers.map((teacher) => {
-    return (
-      <section className="flex w-full items-center flex-col">
-        <div className="w-3/4 justify-center h-20 bg-lightcream grid grid-cols-2 border">
-          <h1 className="border border-black p-2">{teacher.name}</h1>
-          <h1 className="border border-black p-2">{teacher.payroll}</h1>
-        </div>
-      </section>
-    );
-  });
+
+  return (
+    <div>
+      <table className="w-1/2 border border-black">
+        <tr className="text-start">
+          <th>Name</th>
+          <th>Subject</th>
+          <th>Class Type</th>
+          <th>Payroll</th>
+        </tr>
+        {teachers.map((teacher) => {
+          return (
+            <tr className="text-center">
+              <td>{teacher.name}</td>
+              <td>{teacher.subject}</td>
+              <td>{teacher.class_type}</td>
+              <td>{teacher.payroll}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+
+  // return teachers.map((teacher) => {
+  //   return (
+  //     <div>
+  //       <table>
+  //         <thead>
+  //           <tr>Name</tr>
+  //         </thead>
+  //         <tbody>
+  //           <tr>
+  //             <td>teacher.name</td>
+  //           </tr>
+  //         </tbody>
+  //       </table>
+  //     </div>
+  //   );
+  // });
 };
 
 export default PayrollPage;
