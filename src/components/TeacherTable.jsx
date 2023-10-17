e5c00e7d425fb2876ea1448e649717f56d029ba4
yanshin <yan.shin@cdsg.com.mm>
2023-10-17 15:42:48 +0630

    Update payroll page UI for calculations

--- src/components/TeacherTable.jsx
+++ src/components/TeacherTable.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import { useMemo, useState } from "react";
+import { render } from "react-dom";
+import { AgGridReact } from "ag-grid-react";
 
-const TeacherTable = (teacher) => {
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
+
+const TeacherTable = ({ teachers }) => {
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 },
+  ]);
+
+  const [columnDefs] = useState([
+    { field: "name" },
+    { field: "subject" },
+    { field: "class_type", headerName: "Class Type" },
+    { field: "payroll" },
+  ]);
+
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+    filter: true,
+  }));
   return (
-    <div>
-      <h1>{teacher.name}</h1>
-    </div>
+    <section className="flex justify-center">
+      <div
+        className="ag-theme-material bg-center border rounded-md shadow-lg md:w-full"
+        style={{ height: "50vh", width: "60%" }}
+      >
+        <AgGridReact
+          rowData={teachers}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          animateRows={true}
+        ></AgGridReact>
+      </div>
+    </section>
   );
 };
 
--- src/pages/PayrollPage.jsx
+++ src/pages/PayrollPage.jsx
@@ -1,20 +1,40 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import TeacherTable from "../components/TeacherTable";
+import { toast } from "react-toastify";
 
 const PayrollPage = () => {
   const [teachers, setTeachers] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
+  const [calculations, setCalculations] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getPayroll = async () => {
     try {
+      setIsLoading(true);
       const response = await axios.get(
         "http://localhost:5038/api/teachers/pay/all"
       );
-      console.log(response.data.payroll);
+      console.log(teachers);
       setTeachers(response.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const calcPay = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        "http://localhost:5038/api/teachers/calculate/all"
+      );
+      setCalculations(response.data);
+      setIsLoading(false);
+      console.log(calculations.overall);
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
     }
   };
 
@@ -22,45 +42,41 @@ const PayrollPage = () => {
     getPayroll();
   }, []);
 
+  useMemo(() => {
+    calcPay();
+  }, []);
+
+  useMemo(() => calcPay(), []);
+
   return (
     <div>
-      <table className="w-1/2 border border-black">
-        <tr className="text-start">
-          <th>Name</th>
-          <th>Subject</th>
-          <th>Class Type</th>
-          <th>Payroll</th>
-        </tr>
-        {teachers.map((teacher) => {
-          return (
-            <tr className="text-center">
-              <td>{teacher.name}</td>
-              <td>{teacher.subject}</td>
-              <td>{teacher.class_type}</td>
-              <td>{teacher.payroll}</td>
-            </tr>
-          );
-        })}
-      </table>
+      {isLoading ? (
+        <div className="flex justify-center items-center h-screen text-2xl font-bold">
+          Loading Data...
+        </div>
+      ) : (
+        <section className="flex flex-col p-4 ">
+          <TeacherTable teachers={teachers} />;
+          <div className="flex justify-center w-full">
+            <span className="text-md flex flex-col p-2 gap-8 items-center justify-center rounded-xl shadow-lg border bg-lightcream w-[60%] flex-wrap">
+              <h1>
+                {console.log(teachers.overall)}
+                Total Online Classes Payroll:
+                <b> {calculations.onlineTotal} Ks</b>
+              </h1>
+              <h1>
+                Total On-Site Classes Payroll:{" "}
+                <b>{calculations.onsiteTotal} Ks</b>
+              </h1>
+              <h1>
+                Overall All Classes Payroll: <b>{calculations.overall} Ks</b>
+              </h1>
+            </span>
+          </div>
+        </section>
+      )}
     </div>
   );
-
-  // return teachers.map((teacher) => {
-  //   return (
-  //     <div>
-  //       <table>
-  //         <thead>
-  //           <tr>Name</tr>
-  //         </thead>
-  //         <tbody>
-  //           <tr>
-  //             <td>teacher.name</td>
-  //           </tr>
-  //         </tbody>
-  //       </table>
-  //     </div>
-  //   );
-  // });
 };
 
 export default PayrollPage;
